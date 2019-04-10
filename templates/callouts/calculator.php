f25a3ade10a13e198a78a8d1417c5c73c5d174dc
Bryan Stoner <mimoduo@gmail.com>
2019-04-10 17:06:54 -0400

    pricing and contact

---
+++ templates/callouts/calculator.php
@@ -0,0 +1,52 @@
+<div class="calculator" id="calculator">
+  <span class="calculator_icon"><?=icon("paw")?></span>
+  <div class="fs-row">
+    <div class="fs-cell">
+      <div class="calculator_inner">
+        <header class="calculator_header">
+          <h2 class="calculator_title">Calculator</h2>
+        </header>
+        <div class="calculator_body">
+          <p class="calculator_caption"><?=$caption?></p>
+          <div class="calculator_content">
+            <div class="calculator_items">
+              <?php
+								foreach ($items as $item) {
+							?>
+							<div class="calculator_item">
+								<div class="calculator_item_body">
+									<figure class="calculator_item_figure">
+										<div class="lazy_image_wrapper calculator_item_image_wrapper">
+											<img class="js-lazy calculator_item_image js-lazyloaded" data-src="<?=Bigtree::prefixFile($item["image"], "square-sml-")?> alt="">
+										</div>
+									</figure>
+									<div class="calculator_item_content" data-label="<?=$item["label"]?>" data-value="<?=$item["value"]?>">
+										<div class="calculator_item_title">
+											<span class="calculator_item_title_label"><?=$item["label"]?></span>
+											<span class="calculator_item_title_hours">+<?=$item["value"]?> hour<?php if ($item["value"] > 1) {?>s<?php } ?></span>
+										</div>
+										<p class="calculator_item_caption"><?=$item["caption"]?></p>
+										<div class="calculator_item_field">
+											<input class="calculator_item_checkbox" id="<?=$item["label"]?>" type="checkbox" value="<?=$item["label"]?>" />
+											<span class="calculator_item_hint"></span>
+											<label class="calculator_item_label" for="<?=$item["label"]?>">Add/Remove <?=$item["label"]?></label>
+											<span class="calculator_item_action" aria-hidden="true"></span>
+										</div>
+									</div>
+								</div>
+							</div>
+							<?php
+								}
+							?>
+            </div>
+            <div class="calculator_results">
+              <div class="calculator_results_body">
+              </div>
+              <a class="calculator_results_link" href="#"></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
