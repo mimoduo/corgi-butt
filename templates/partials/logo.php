1698de57ddc15cbe4594a7777c141799c368dad2
Bryan Stoner <mimoduo@gmail.com>
2019-07-19 13:20:39 -0400

    amazing

--- templates/partials/logo.php
+++ templates/partials/logo.php
@@ -16,7 +16,7 @@
 <div class="<?=implode(" ", $logo_classes)?>" itemscope itemtype="http://schema.org/<?=$site["schema_type"]?>">
 	<a class="logo_link" itemprop="url" href="<?=$link?>">
 		<span class="logo_link_icon">
-			<img class="logo_link_image" src="<?=STATIC_ROOT?>images/logo.svg" alt="" />
+			<img class="logo_link_image" src="<?=STATIC_ROOT?>images/logo.svg" alt="a big corgi butt" />
 		</span>
 		<span class="logo_link_label"><?=$site["title"]?></span>
 	</a>
