b4b4113211060ba4d384d1d768ee5d92a5d02a4a
Bryan Stoner <mimoduo@gmail.com>
2019-07-19 14:26:54 -0400

    minify

--- site/src/js/modules/calculator.js
+++ site/src/js/modules/calculator.js
@@ -13,18 +13,16 @@ Site.modules.Calculator = (function($, Site) {
 	function init() {
 		if ($(".calculator").length) {
 			$results = $(".calculator_results");
-			base = `
-				<div class="calculator_result_item">
-					<span class="calculator_result_item_label">Basic Website</span>
-					<span class="calculator_result_item_value">+10</span>
-				</div>
-			`;
-			content = `
-				<div class="calculator_result_item">
-					<span class="calculator_result_item_label">Total</span>
-					<span class="calculator_result_item_value">$800</span>
-				</div>
-			`;
+			base = '<div class="calculator_result_item">' +
+				'<span class="calculator_result_item_label">Basic Website</span>' +
+				'<span class="calculator_result_item_value">+10</span>' +
+			'</div>';
+
+			content = '<div class="calculator_result_item">' +
+				'<span class="calculator_result_item_label">Total</span>' +
+				'<span class="calculator_result_item_value">$800</span>' +
+			'</div>';
+
 			total = 0;
 			
 			updatePrice();
@@ -44,12 +42,10 @@ Site.modules.Calculator = (function($, Site) {
 			if ($(this).is(":checked")) {
 				var item = $(this).closest(".calculator_item_content");
 
-				content += `
-					<div class="calculator_result_item">
-						<span class="calculator_result_item_label">${$(item).data("label")}</span>
-						<span class="calculator_result_item_value">+${$(item).data("value")}</span>
-					</div>
-				`;
+				content += '<div class="calculator_result_item">' +
+					'<span class="calculator_result_item_label">' + $(item).data("label") + '</span>' +
+					'<span class="calculator_result_item_value">' + $(item).data("value") + '</span>' +
+				'</div>';
 
 				total += $(item).data("value");
 			}
@@ -57,12 +53,10 @@ Site.modules.Calculator = (function($, Site) {
 
 		total += 10;
 
-		content += `
-			<div class="calculator_result_item">
-				<span class="calculator_result_item_label">Total</span>
-				<span class="calculator_result_item_value">$80 * ${total} Hours = <strong>$${total * 80}</strong></span>
-			</div>
-		`;
+		content += '<div class="calculator_result_item">' +
+			'<span class="calculator_result_item_label">Total</span>' +
+			'<span class="calculator_result_item_value">$80 * ' + total + ' Hours = <strong>$' + total * 80 + '</strong></span>' +
+		'</div>';
 
 		$results.html(base + content);
 	}
